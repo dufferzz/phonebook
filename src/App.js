590f5f776747f6ca4b5d60e000dd71ca9f2e7a04
dufferzz <madcrafter0@gmail.com>
2020-12-05 18:24:40 +0100

    Small refactor, Prettier, ES6-ify

--- src/App.js
+++ src/App.js
@@ -1,106 +1,111 @@
+import React, { useState } from 'react';
 
-import React, {useState} from 'react'
+const InputForm = ({ book, addToPhoneBook }) => {
+  const [firstName, setFirstName] = useState('Some');
+  const [lastName, setLastName] = useState('DefaultValues');
+  const [phoneNumber, setPhoneNumber] = useState('9999999');
 
-function InputForm({book, addToPhoneBook}) {
-  const [firstName, setFirstName] = useState('Some')
-  const [lastName, setLastName] = useState('DefaultValues')
-  const [phoneNumber, setPhoneNumber] = useState('9999999')
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
 
-  const handleFirstNameChange = (e) =>{
-    setFirstName(e.target.value)
-  }
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  };
 
-  const handleLastNameChange = (e) =>{
-    setLastName(e.target.value)
-  }
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+  };
 
-  const handlePhoneNumberChange = (e) =>{
-    setPhoneNumber(e.target.value)
-  }
-
-  function addToTable(e){
+  const addToTable = (e) => {
     e.preventDefault();
 
-    if(firstName === '' || lastName === '' || phoneNumber === ''){
-      alert('Please suppply all values!')
+    if (firstName === '' || lastName === '' || phoneNumber === '') {
+      alert('Please suppply all values!');
       return;
     }
-    addToPhoneBook([...book, {firstName, lastName, phoneNumber}])
-    setLastName('')
-    setFirstName('')
-    setPhoneNumber('')
-  }
+    addToPhoneBook([...book, { firstName, lastName, phoneNumber }]);
+    setLastName('');
+    setFirstName('');
+    setPhoneNumber('');
+  };
 
-  return(
+  return (
     <div>
       <form onSubmit={addToTable}>
-        <input 
+        <input
           value={firstName}
           placeholder="First Name"
           onChange={handleFirstNameChange}
           type="text"
-          name="firstName"></input>
+          name="firstName"
+        ></input>
 
-        <input value={lastName}
+        <input
+          value={lastName}
           placeholder="Last Name"
           onChange={handleLastNameChange}
           type="text"
-          name="lastName"></input>
+          name="lastName"
+        ></input>
 
-        <input value={phoneNumber}
+        <input
+          value={phoneNumber}
           placeholder="Phone Number"
           onChange={handlePhoneNumberChange}
           type="text"
-          name="phoneNumber"></input>
+          name="phoneNumber"
+        ></input>
 
         <button>Add</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-function Table(props)
-{
-  return(
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <td>First name</td>
-            <td>Last Name</td>
-            <td>Phone Number</td>
-          </tr>
-        </thead>
-        <tbody>
-          {props.data.sort((a, b) => {
-      if (a.lastName.toLowerCase() < b.lastName.toLowerCase()) 
-          return -1 
-      if (a.lastName.toLowerCase() > b.lastName.toLowerCase())
-          return 1
-      return 0
-  }).map((person, index)=>(
-            <tr key={index}>
-            <td>{person.firstName}</td>
-            <td>{person.lastName}</td>
-            <td>{person.phoneNumber}</td>
-          </tr>
-          ))}
-          
-        </tbody>
-      </table>
-    </div>
-  )
-}
-
-
-function App() {
-  const [phoneBook, addToPhoneBook] = useState([])
+const BookItem = ({ person, index }) => {
   return (
-    <div className="App">
-      <InputForm book={phoneBook} addToPhoneBook={addToPhoneBook}/>
-      <Table data={phoneBook}/>
+    <tr key={index}>
+      <td>{person.firstName}</td>
+      <td>{person.lastName}</td>
+      <td>{person.phoneNumber}</td>
+    </tr>
+  );
+};
+
+const Table = ({ data }) => {
+  const sortedTable = data.sort((a, b) => {
+    if (a.lastName.toLowerCase() < b.lastName.toLowerCase()) return -1;
+    if (a.lastName.toLowerCase() > b.lastName.toLowerCase()) return 1;
+    return 0;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>First name</td>
+          <td>Last Name</td>
+          <td>Phone Number</td>
+        </tr>
+      </thead>
+      <tbody>
+        {sortedTable.map((person, index) => (
+          <BookItem key={index} person={person} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const App = () => {
+  const [phoneBook, addToPhoneBook] = useState([]);
+  return (
+    <div>
+      <InputForm book={phoneBook} addToPhoneBook={addToPhoneBook} />
+      <Table data={phoneBook} />
     </div>
   );
-}
+};
 
 export default App;
